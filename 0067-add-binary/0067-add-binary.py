b95981ce361bad85ca9c192c505f10b14fbc8c7f
NikhilY-HUB <nikfx29@gmail.com>
2026-05-22 13:03:55 +0530

    Time: 4 ms (58.69%), Space: 12.3 MB (58.8%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -5,4 +5,18 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        return bin(int(a,2) + int(b, 2))[2:]
+        res = ""
+        carry = 0
+        i = len(a) - 1
+        j = len(b) - 1
+        while i>=0 or j >= 0 or carry > 0:
+            total = carry
+            if i>=0:
+                total += int(a[i])
+                i -= 1
+            if j >=0:
+                total += int(b[j])
+                j -= 1
+            res = str(total % 2) + res
+            carry = total // 2
+        return res
